fa82ff21454afc7cfadb8f048462bfb88bff8f36
Ed <edvinas2225@gmail.com>
2023-12-06 20:45:29 +0000

    Create Glassmorphic Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './Home'
 import About from './About'
+import Navbar from './Navbar'
 
 function App() {
 
   return (
     <>
+     <Navbar/>
      <Home/>
      <About/>
     </>
---
+++ src/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Expierence</li>
+            <li>Skills</li>
+            <li>Contact Me</li>
+        </div>
+      );
+}
+ 
+export default Navbar;
