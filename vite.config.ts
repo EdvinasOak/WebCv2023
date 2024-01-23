cf0d6c331338f6661f5346a5398f7c991c8b1154
EdvinasOak <edvinas2225@gmail.com>
2024-01-23 22:35:45 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/WebCV2023/',
+	base: '/WebCv2023/',
 	css: {
 		modules: {
 			localsConvention: 'camelCase',
